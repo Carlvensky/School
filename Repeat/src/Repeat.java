18ab8d851b3445da99e59f494626c862ad2d82e4
Carlvensky St Fleur <132518428+Carlvensky@users.noreply.github.com>
2023-10-21 18:43:47 +0000

    Oct 21

---
+++ Repeat/src/Repeat.java
@@ -0,0 +1,7 @@
+public class Repeat {
+    public static void main(String[] args)  {
+        // Display the message five times
+        for (int i =0; i < 5; i++)
+        System.out.println("Welcome to Java!");
+    }
+}
