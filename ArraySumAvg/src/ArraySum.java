deca58f7fe8be079b8c85ae212b225e6dbba3405
Carlvensky St Fleur <132518428+Carlvensky@users.noreply.github.com>
2025-01-22 16:53:12 +0000

    Integrate scanner to received user input.

--- ArraySumAvg/src/ArraySum.java
+++ ArraySumAvg/src/ArraySum.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ArraySum {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner numberInt = new Scanner(System.in);
+        System.out.print("How many integers do you want to enter?");
     }
 }
