68b310563a93347221b831a8c552c7d3d95ae9f3
Carlvensky St Fleur <132518428+Carlvensky@users.noreply.github.com>
2025-01-24 03:22:53 +0000

    Complete.

--- ArraySumAvg/src/ArraySum.java
+++ ArraySumAvg/src/ArraySum.java
@@ -3,15 +3,34 @@ import java.util.Scanner;
 public class ArraySum {
     public static void main(String[] args) throws Exception {
 
-        int[] array;
-
         Scanner scanner = new Scanner(System.in);
 
         int n = 0;
+
+        //A do while loop to ensure the user enter an integer greater than 0
         do {
             System.out.print("How many integers do you want to enter? ");
             n = scanner.nextInt();
         } while (n <= 0);
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Please enter one of your " + n + " integers ");
+            array[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int num: array) {
+            sum += num;
+        }
+
+        double avg = (double) sum / n;
+
+        //Adding an empty line for better readability
+        System.out.println("");
+        System.out.println("The sum of your " + n + " integers is " + sum);
+        System.out.println("The average of your " + n + " integers is " + avg);
 
         scanner.close();
         
