35f44a9c605f3bd82ddbc670720a9a1e4b5660ed
Carlvensky St Fleur <132518428+Carlvensky@users.noreply.github.com>
2025-01-28 23:57:30 +0000

    Able to search in the Array

--- SearchArray/src/Search.java
+++ SearchArray/src/Search.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Search {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Initialize numbers and # the user will search
+        int[] numbers = {11, 82, 11, 16, 4, 7};
+        int search = 0;
+        
+        //Use scanner to get user input and assign it to search
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to search for: ");
+        search = scanner.nextInt();
+
+        //for loop to search for the number if found break out of the loop
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == search) {
+                System.out.println("Number found at index " + i);
+                break;
+            } else {
+                System.out.println("Number not found");
+                break;
+            }
+        }
+        scanner.close();
     }
 }
