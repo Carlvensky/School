eaa49161c5d300ff73b3d0b5ae78ea8c385bfacb
Carlvensky St Fleur <132518428+Carlvensky@users.noreply.github.com>
2025-02-11 00:14:33 +0000

    Inheritance

---
+++ Animal-dog/src/Main.java
@@ -0,0 +1,20 @@
+//the time complexity for this is O(1)
+class Animal {
+    void makeSound() {
+        System.out.println("Generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Woof!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.makeSound(); // Output: Woof!
+    }
+}
